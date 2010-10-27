9d199b0be8756529e93e3ddee202d55998e17db7
Sean Carey <sean@densone.com>
2010-10-26 23:04:34 -0400

    cleanup etc

--- cookbooks/shorewall/metadata.rb
+++ cookbooks/shorewall/metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "Example Com"
-maintainer_email "ops@example.com"
+maintainer       "dens.one"
+maintainer_email "sean@densone.com"
 license          "Apache 2.0"
 description      "Installs/Configures firewall"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
